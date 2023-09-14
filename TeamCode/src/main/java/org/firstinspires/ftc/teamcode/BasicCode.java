12c63a985a2723c5906c8b90dc451a3d7c597b8c
Ryanver07 <62667041+Ryanver07@users.noreply.github.com>
2023-09-14 06:54:00 -0400

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicCode.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class BasicCode extends LinearOpMode {
+    
+}
