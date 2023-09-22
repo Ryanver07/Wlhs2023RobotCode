1535a09e938e0faace5bc7d6cd70e851a3a7c8b5
Ryanver07 <62667041+Ryanver07@users.noreply.github.com>
2023-09-22 11:19:35 -0400

    Version .12

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DualAutoMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DualAutoMode.java
@@ -3,10 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import org.checkerframework.checker.initialization.qual.Initialized;
-import org.firstinspires.ftc.robotcore.external.Telemetry;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import org.checkerframework.checker.initialization.qual.Initialized;
+//import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp(name = "DualAutoMode", group = "Team 13463 (WLHS)")
 public class DualAutoMode extends LinearOpMode{
